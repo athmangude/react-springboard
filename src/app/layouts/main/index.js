cc2e10fd59fc417bd337752d528076d2c8e983f1
Athman <athmangude@gmail.com>
2019-03-05 22:32:13 +0300

    updated layout

--- src/app/layouts/main/index.js
+++ src/app/layouts/main/index.js
@@ -7,8 +7,8 @@ export default class MainLayout extends Component {
   render() {
     return (
       <div style={{ width: '100%' }}>
-        <div style={{ height: 64, width: 'calc(100% - 20px)', backgroundColor: '#008080', display: 'flex', alignItems: 'center', justifyContent: 'flex-start', padding: 10 }}>
-          <Link to="/" style={{ color: '#fff', fontSize: 20, textDecoration: 'none', textTransform: 'uppercase', fontWeight: 'bold' }}>SpringBoard</Link>
+        <div style={{ height: 64, width: '100%', backgroundColor: '#008080', display: 'flex', alignItems: 'center', justifyContent: 'flex-start' }}>
+          <Link to="/" style={{ color: '#fff', fontSize: 20, textDecoration: 'none', textTransform: 'uppercase', fontWeight: 'bold', marginLeft: 10 }}>SpringBoard</Link>
         </div>
         <div style={{ height: 'calc(100vh - 64px)', width: '100%', overflow: 'auto' }}>
           {this.props.children}
