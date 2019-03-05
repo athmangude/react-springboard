b13f5a5bf6236bc33ed89064ba7813553ed4002b
Athman <athmangude@gmail.com>
2019-03-05 12:24:48 +0300

    connected landing to the redux store

--- src/app/containers/Landing/index.js
+++ src/app/containers/Landing/index.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
 import './index.css';
 
 import usabilityImage from 'Images/usability.png';
 
+@connect((state) => ({
+  ...state
+}), (dispatch) => ({
+
+}))
 export default class Landing extends Component {
   render() {
+    console.log('[this.props]', this.props);
     return (
       <div>
         <img src={usabilityImage} style={{ width: '100%' }} />
