9587239bb09da4ea295d162742d8e142a6e0c460
Athman <athmangude@gmail.com>
2019-03-06 11:49:55 +0300

    added meta tags

--- src/app/layouts/main/index.js
+++ src/app/layouts/main/index.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import styled, { ThemeProvider } from 'styled-components';
+import { Helmet } from "react-helmet";
 
 import themes from '../../themes';
 
@@ -20,10 +21,15 @@ const MainLayoutWrapper = styled.main`${styles}`;
 }))
 export default class MainLayout extends Component {
   render() {
+    const theme = themes.tealLime;
     console.log(this.props);
     return (
-      <ThemeProvider theme={themes.tealLime}>
+      <ThemeProvider theme={theme}>
         <MainLayoutWrapper style={{ width: '100%' }}>
+          <Helmet>
+            <meta name="theme-color" content={theme.secondaryColor} />
+            <meta name="background-color" content={theme.darkPrimaryColor} />
+          </Helmet>
           <aside className="main">
             <Link to="/" style={{ color: '#fff', fontSize: 25, textDecoration: 'none', textTransform: 'uppercase', fontWeight: 'bold', marginLeft: 10 }}>Spring Board</Link>
           </aside>
