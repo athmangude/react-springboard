868ea755c202a037646e2189975a34d7badb5372
Athman <athmangude@gmail.com>
2019-03-06 10:53:50 +0300

    added themes

---
+++ src/app/themes.js
@@ -0,0 +1,24 @@
+export default {
+  light: {
+    // https://material.io/tools/color/#!/?view.left=0&view.right=0&primary.color=9E9E9E&secondary.color=212121
+    primaryColor: '#9e9e9e',
+    lightPrimaryColor: '#cfcfcf',
+    darkPrimaryColor: '#707070',
+    primaryTextColor: '#000000',
+    secondaryColor: '#212121',
+    lightSecondaryColor: '#484848',
+    darkSecondaryColor: '#000000',
+    secondaryTextColor: '#ffffff',
+  },
+  dark: {
+    // https://material.io/tools/color/#!/?view.left=0&view.right=0&primary.color=212121&secondary.color=212121
+    primaryColor: '#212121',
+    lightPrimaryColor: '#484848',
+    darkPrimaryColor: '#000000',
+    primaryTextColor: '#ffffff',
+    secondaryColor: '#212121',
+    lightSecondaryColor: '#484848',
+    darkSecondaryColor: '#000000',
+    secondaryTextColor: '#ffffff',
+  }
+}
