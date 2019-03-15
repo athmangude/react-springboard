3503cc3ae23124ead09c265320f016dabcc18df4
Athman <athmangude@gmail.com>
2019-03-15 16:15:49 +0300

    implemented demo of counter

--- src/app/containers/Landing/index.js
+++ src/app/containers/Landing/index.js
@@ -16,11 +16,20 @@ export default class Landing extends Component {
     counter: 0,
   }
 
+  onButtonPressed = () => {
+    let { counter } = this.state;
+    this.setState({
+      counter: ++counter,
+    });
+  }
+
   render() {
+    const { counter } = this.state;
     return (
       <section style={{ margin: 10 }}>
         <h1>Build something amazing</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <button onClick={this.onButtonPressed}>{`Pressed ${counter} times`}</button>
       </section>
     );
   }
