6cd4763ca24033f63674c7659c4318b97e4a7f99
Athman Gude <athmangude@gmail.com>
2019-12-28 17:33:03 +0300

    added app theme for main app

---
+++ src/modules/main/theme.js
@@ -0,0 +1,13 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import blue from '@material-ui/core/colors/blue';
+import yellow from '@material-ui/core/colors/yellow';
+
+export default () => createMuiTheme({
+    palette: {
+        primary: blue,
+        secondary: yellow,
+    },
+    status: {
+        danger: 'red',
+    },
+});
