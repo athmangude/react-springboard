7a81969127078d8444b622e440cde515758fb0a7
Athman Gude <athmangude@gmail.com>
2020-01-08 11:40:25 +0300

    updated manifest and icons

--- src/layouts/simple-layout-extended/components/side-navigation/AppInstallButton.js
+++ src/layouts/simple-layout-extended/components/side-navigation/AppInstallButton.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import manifest from 'Src/assets/manifest.json';
+
 import styles from './AppInstallButton.css';
 
 const AppInstallButtonWrapper = styled.button`${styles}`;
 
 const AppInstallButton = ({ onInstallButtonClicked }) => {
+  console.log(manifest);
   return (
     <AppInstallButtonWrapper
       onClick={onInstallButtonClicked}
     >
       <i className="material-icons" style={{ fontSize: 20, margin: '0 10px' }}>get_app</i>
-      <span>Install Spring Board</span>
+      <span>Install {manifest.name}</span>
     </AppInstallButtonWrapper>
   )
 }
