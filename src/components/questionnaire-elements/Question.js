b8976126e7cb472272f1a0448070eaaa83ffdb89
Athman Gude <athmangude@gmail.com>
2020-01-13 15:44:50 +0300

    setting values of invisible questions to undefined

--- src/components/questionnaire-elements/Question.js
+++ src/components/questionnaire-elements/Question.js
@@ -2,7 +2,7 @@
 /* eslint-disable react/require-default-props */
 /* eslint-disable no-nested-ternary */
 /* eslint-disable react/forbid-prop-types */
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import Typography from '@material-ui/core/Typography';
@@ -13,64 +13,77 @@ import { Text, Number, Radio, Checkbox, Slider, DatePicker } from './input-types
 const QuestionWrapper = styled.div`${styles}`;
 
 const Question = ({ question, onChange, response, parent, parentValue, validation }) => {
-  let show = false;
-  if (!question.parentTag) {
-    show = true;
-  } else if (parent.inputType === 'RADIO') {
-    if ('showIfParentValueEquals' in question) {
-      if (parentValue === question.showIfParentValueEquals) { // eslint-disable-line max-len
-        show = true;
+  function isVisible() {
+    let show = false;
+    if (!question.parentTag) {
+      show = true;
+    } else if (parent.inputType === 'RADIO') {
+      if ('showIfParentValueEquals' in question) {
+        if (parentValue === question.showIfParentValueEquals) { // eslint-disable-line max-len
+          show = true;
+        }
+      } else if ('showIfParentValueNotEquals' in question) {
+        if (parentValue && parentValue !== question.showIfParentValueNotEquals) { // eslint-disable-line max-len
+          show = true;
+        }
       }
-    } else if ('showIfParentValueNotEquals' in question) {
-      if (parentValue && parentValue !== question.showIfParentValueNotEquals) { // eslint-disable-line max-len
-        show = true;
-      }
-    }
-  } else if (parent.inputType === 'CHECKBOX') {
-    if ('showIfParentValueIncludes' in question) {
-      if (parentValue != null && parentValue.length > 0) {
-        show = parentValue.includes(question.showIfParentValueIncludes);
-      }
-    } else {
-      if ('showIfParentValueNotIncludes' in question) {
-        if (question.showIfParentValueNotIncludes != null && question.showIfParentValueNotIncludes.length > 0) {
-          if (Array.isArray(question.showIfParentValueNotIncludes)) {
-            if (parentValue != null && parentValue.length > 0) {
-              const item = lodash.intersection(question.showIfParentValueNotIncludes, parentValue);
-              show = !(item.length > 0);
-            }
-          } else {
-            // Defined as a string
-            if (parentValue != null && parentValue.length > 0) {
-              show = !(parentValue && parentValue.includes(question.showIfParentValueNotIncludes));
+    } else if (parent.inputType === 'CHECKBOX') {
+      if ('showIfParentValueIncludes' in question) {
+        if (parentValue != null && parentValue.length > 0) {
+          show = parentValue.includes(question.showIfParentValueIncludes);
+        }
+      } else {
+        if ('showIfParentValueNotIncludes' in question) {
+          if (question.showIfParentValueNotIncludes != null && question.showIfParentValueNotIncludes.length > 0) {
+            if (Array.isArray(question.showIfParentValueNotIncludes)) {
+              if (parentValue != null && parentValue.length > 0) {
+                const item = lodash.intersection(question.showIfParentValueNotIncludes, parentValue);
+                show = !(item.length > 0);
+              }
+            } else {
+              // Defined as a string
+              if (parentValue != null && parentValue.length > 0) {
+                show = !(parentValue && parentValue.includes(question.showIfParentValueNotIncludes));
+              }
             }
           }
         }
       }
+    } else if (parent.inputType === 'REMOTE_QUERY') {
+      if ('showIfParentValueEquals' in question) {
+        if ((parentValue !== undefined) && parentValue === question.showIfParentValueEquals) { // eslint-disable-line max-len
+          show = true;
+        }
+      } else if ('showIfParentValueNotEquals' in question) {
+        if ((parentValue !== null) && parentValue !== question.showIfParentValueNotEquals) { // eslint-disable-line max-len
+          show = true;
+        }
+      }
+    } else if (parent.inputType === 'NUMBER') {
+      if ('showIfParentValueEquals' in question) {
+        if (parentValue === question.showIfParentValueEquals) { // eslint-disable-line max-len
+          show = true;
+        }
+      } else if ('showIfParentValueNotEquals' in question) {
+        if (parentValue && parentValue !== question.showIfParentValueNotEquals) { // eslint-disable-line max-len
+          show = true;
+        }
+      }
     }
-  } else if (parent.inputType === 'REMOTE_QUERY') {
-    if ('showIfParentValueEquals' in question) {
-      if ((parentValue !== undefined) && parentValue === question.showIfParentValueEquals) { // eslint-disable-line max-len
-        show = true;
-      }
-    } else if ('showIfParentValueNotEquals' in question) {
-      if ((parentValue !== null) && parentValue !== question.showIfParentValueNotEquals) { // eslint-disable-line max-len
-        show = true;
-      }
-    }
-  } else if (parent.inputType === 'NUMBER') {
-    if ('showIfParentValueEquals' in question) {
-      if (parentValue === question.showIfParentValueEquals) { // eslint-disable-line max-len
-        show = true;
-      }
-    } else if ('showIfParentValueNotEquals' in question) {
-      if (parentValue && parentValue !== question.showIfParentValueNotEquals) { // eslint-disable-line max-len
-        show = true;
-      }
-    }
+
+    return show;
   }
 
-  if (show) {
+  useEffect(() => {
+    return () => {
+      // IDEA: check whether a question that should NOT be visible has a value and set it to null
+      if (!isVisible() && response) {
+        onChange(question.tag, null);
+      }
+    }
+  });
+
+  if (isVisible()) {
     let validationRules;
     let validationErrors;
 
